853a6a2656fd2788148ff0d2c1d5f61b730822d6
Edlingao <Edwincosmos32@hotmail.com>
2019-06-10 19:24:34 -0500

    Finished edit user feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates :email, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     
 
